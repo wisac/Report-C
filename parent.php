f109d8851903922e15c01a178f860ef751dd307a
Isaac Wilson <iswilson.dev@gmail.com>
2023-09-01 12:14:05 +0000

    update: Improve parent portal style

--- parent.php
+++ parent.php
@@ -6,136 +6,141 @@ include('includes/config.php'); ?>
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>ReportC | Results</title>
-    <link rel="icon" type="image/x-icon" href="images/favicon.ico">
-    <link rel="stylesheet" href="css/bootstrap.min.css" media="screen">
-    <link rel="stylesheet" href="css/font-awesome.min.css" media="screen">
-    <link rel="stylesheet" href="css/animate-css/animate.min.css" media="screen">
-    <link rel="stylesheet" href="css/icheck/skins/flat/blue.css">
-    <link rel="stylesheet" href="css/new.css">
-    <link rel="stylesheet" href="css/background.css" media="screen">
-    <link rel="stylesheet" href="css/main.css" media="screen">
-   
-    <script src="js/modernizr/modernizr.min.js"></script>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>ReportC | Results</title>
+  <link rel="icon" type="image/x-icon" href="images/favicon.ico">
+  <link rel="stylesheet" href="css/bootstrap.min.css" media="screen">
+  <link rel="stylesheet" href="css/font-awesome.min.css" media="screen">
+  <link rel="stylesheet" href="css/animate-css/animate.min.css" media="screen">
+  <link rel="stylesheet" href="css/icheck/skins/flat/blue.css">
+  <link rel="stylesheet" href="css/new.css">
+  <link rel="stylesheet" href="css/background.css" media="screen">
+  <link rel="stylesheet" href="css/main.css" media="screen">
+
+  <script src="js/modernizr/modernizr.min.js"></script>
 </head>
 
 <body>
 
-    
-    <img class="bg-img" src="images/parents_play_graound.jpg">
-    <div class="title-container" style="z-index: 5; position: absolute; right: 0; left: 0">
-                    <div class="logo-box">
-                        <!-- <h1 class="logo-title" align="">REPORTC</h1> -->
-                        <a href="index.php"><img id="logo-img" src="images/REPORTC-logo.png" alt="logo"></a>
-                    </div>
-                    <div class="school-box">
 
-                        <h1 class="school-name" align="center">Ashaiman Mandela School</h1>
-                    </div>
-                    <div class="portal-container">
-                        <a class="teacher-portal portal-link" href="teachers.php">Teachers</a>
-                        <a class="parent-portal portal-link" href="find-result.php">Students</a>
-                        <a class="parent-portal portal-link" href="parent.php">Parents</a>
+  <img class="bg-img" src="images/parents_play_graound.jpg">
+  <div class="title-container" style="z-index: 5; position: absolute; right: 0; left: 0">
+    <div class="logo-box">
+      <!-- <h1 class="logo-title" align="">REPORTC</h1> -->
+      <a href="index.php"><img id="logo-img" src="images/REPORTC-logo.png" alt="logo"></a>
+    </div>
+    <div class="school-box" style="display: inline">
 
-                    </div>
+      <h1 class="school-name" align=" center">Ashaiman Mandela School</h1>
+    </div>
+    <div class="portal-container">
+      <a class="teacher-portal portal-link" href="teachers.php">Teachers</a>
+      <a class="parent-portal portal-link" href="find-result.php">Students</a>
+      <a class="parent-portal portal-link" href="parent.php">Parents</a>
+
+    </div>
+  </div>
+  <div class="main-wrapper">
+
+    <div class="login-bg-color bg-black-300">
+      <div style="height: 100vh; width: 100vw; background-color: rgba(0,0,0,0.70); position: absolute; z-index: 0">
+
+      </div>
+
+
+      <div class="row">
+
+        <div class="col-md-4 col-md-offset-4">
+          <div class="panel login-box" style="opacity: 100">
+            <div class="panel-heading">
+              <div class="panel-title text-center">
+                <h4><b>Parents Portal.</b></h4>
+                <h4>Check your kid's results here</h4>
+              </div>
+            </div>
+            <div class="panel-body p-20">
+
+
+
+              <form action="result.php" method="post">
+                <div class="form-group">
+                  <label for="rollid">Enter your kids Student ID</label>
+                  <input type="text" class="form-control" id="rollid" placeholder="Enter Your Kids Student ID"
+                    autocomplete="off" name="rollid">
                 </div>
-    <div class="main-wrapper">
-
-        <div class="login-bg-color bg-black-300">
-            
-            <div class="row">
-                
-                <div class="col-md-4 col-md-offset-4">
-                    <div class="panel login-box" style="opacity: 80%">
-                        <div class="panel-heading">
-                            <div class="panel-title text-center">
-                            <h4><b>Parents Portal.</b></h4>
-                                <h4>Check your kid's results here</h4>
-                            </div>
-                        </div>
-                        <div class="panel-body p-20" >
-
-
-
-                            <form action="result.php" method="post">
-                                <div class="form-group">
-                                    <label for="rollid">Enter your kids Student ID</label>
-                                    <input type="text" class="form-control" id="rollid" placeholder="Enter Your Kids Student ID"
-                                        autocomplete="off" name="rollid">
-                                </div>
-                                <div class="form-group">
-                                    <label for="default" class="col-sm-2 control-label">Class</label>
-                                    <select name="class" class="form-control" id="default" required="required">
-                                        <option disabled selected="true" value="">Select Class</option>
-                                        <?php $sql = "SELECT * from tblclasses";
+                <div class="form-group">
+                  <label for="default" class="col-sm-2 control-label">Class</label>
+                  <select name="class" class="form-control" id="default" required="required">
+                    <option disabled selected="true" value="">Select Class</option>
+                    <?php $sql = "SELECT * from tblclasses";
                                         $query = $dbh->prepare($sql);
                                         $query->execute();
                                         $results = $query->fetchAll(PDO::FETCH_OBJ);
                                         if ($query->rowCount() > 0) {
                                             foreach ($results as $result) { ?>
-                                                <option value="<?php echo htmlentities($result->id); ?>"><?php echo htmlentities($result->ClassName); ?>&nbsp; Section-<?php echo htmlentities($result->Section); ?></option>
-                                            <?php }
+                    <option value="<?php echo htmlentities($result->id); ?>">
+                      <?php echo htmlentities($result->ClassName); ?>&nbsp;
+                      Section-<?php echo htmlentities($result->Section); ?></option>
+                    <?php }
                                         } ?>
-                                    </select>
-                                </div>
-
-
-                                <div class="form-group mt-20">
-                                    <div class="">
-
-                                        <button type="submit" class="btn btn-success btn-labeled pull-right">Search<span
-                                                class="btn-label btn-label-right"><i
-                                                    class="fa fa-check"></i></span></button>
-                                        <div class="clearfix"></div>
-                                    </div>
-                                </div>
-
-                                <div class="col-sm-6">
-                                    <a href="index.php">Back to Home</a>
-                                </div>
-                            </form>
-
-                            <hr>
-
-                        </div>
-                    </div>
-                    <!-- /.panel -->
-                    <p class="text-muted text-center"><small>Copyright © 2023 REPORTC</small></p>
+                  </select>
                 </div>
-                <!-- /.col-md-6 col-md-offset-3 -->
+
+
+                <div class="form-group mt-20">
+                  <div class="">
+
+                    <button type="submit" class="btn btn-success btn-labeled pull-right">Search<span
+                        class="btn-label btn-label-right"><i class="fa fa-check"></i></span></button>
+                    <div class="clearfix"></div>
+                  </div>
+                </div>
+
+                <div class="col-sm-6">
+                  <a href="index.php">Back to Home</a>
+                </div>
+              </form>
+
+              <hr>
+
             </div>
-            <!-- /.row -->
+          </div>
+          <!-- /.panel -->
+          <p class="text-muted text-center"><small>Copyright © 2023 REPORTC</small></p>
         </div>
-        <!-- /. -->
-
+        <!-- /.col-md-6 col-md-offset-3 -->
+      </div>
+      <!-- /.row -->
     </div>
-    <!-- /.main-wrapper -->
+    <!-- /. -->
 
-    <!-- ========== COMMON JS FILES ========== -->
-    <script src="js/jquery/jquery-2.2.4.min.js"></script>
-    <script src="js/jquery-ui/jquery-ui.min.js"></script>
-    <script src="js/bootstrap/bootstrap.min.js"></script>
-    <script src="js/pace/pace.min.js"></script>
-    <script src="js/lobipanel/lobipanel.min.js"></script>
-    <script src="js/iscroll/iscroll.js"></script>
+  </div>
+  <!-- /.main-wrapper -->
 
-    <!-- ========== PAGE JS FILES ========== -->
-    <script src="js/icheck/icheck.min.js"></script>
+  <!-- ========== COMMON JS FILES ========== -->
+  <script src="js/jquery/jquery-2.2.4.min.js"></script>
+  <script src="js/jquery-ui/jquery-ui.min.js"></script>
+  <script src="js/bootstrap/bootstrap.min.js"></script>
+  <script src="js/pace/pace.min.js"></script>
+  <script src="js/lobipanel/lobipanel.min.js"></script>
+  <script src="js/iscroll/iscroll.js"></script>
 
-    <!-- ========== THEME JS ========== -->
-    <script src="js/main.js"></script>
-    <script>
-        $(function () {
-            $('input.flat-blue-style').iCheck({
-                checkboxClass: 'icheckbox_flat-blue'
-            });
-        });
-    </script>
+  <!-- ========== PAGE JS FILES ========== -->
+  <script src="js/icheck/icheck.min.js"></script>
 
-    <!-- ========== ADD custom.js FILE BELOW WITH YOUR CHANGES ========== -->
+  <!-- ========== THEME JS ========== -->
+  <script src="js/main.js"></script>
+  <script>
+  $(function() {
+    $('input.flat-blue-style').iCheck({
+      checkboxClass: 'icheckbox_flat-blue'
+    });
+  });
+  </script>
+
+  <!-- ========== ADD custom.js FILE BELOW WITH YOUR CHANGES ========== -->
 </body>
 
 </html>
